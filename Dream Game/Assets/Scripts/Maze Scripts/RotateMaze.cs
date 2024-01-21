1d440d7eb9563434a851a59d28da3af286d93647
camilee03 <crlee15@asu.edu>
2024-01-21 13:18:16 -0700

    Finished Portal System
    - Created a portal that transitions between scenes
    - Refined the rotation system

--- Dream Game/Assets/Scripts/Maze Scripts/RotateMaze.cs
+++ Dream Game/Assets/Scripts/Maze Scripts/RotateMaze.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class RotateMaze : MonoBehaviour
 {
-    private float rotation;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow)) { 
-            if (rotation < .5f) { rotation += .05f; }
+            this.transform.Rotate(0, 0, 2f);
         }
             
         else if (Input.GetKeyDown(KeyCode.RightArrow)) { 
-            if (rotation > -.5f) { rotation -= .05f; }
+            this.transform.Rotate(0, 0, -2f);
         }
 
-        this.transform.Rotate(0, 0, rotation);
+        
     }
 }
