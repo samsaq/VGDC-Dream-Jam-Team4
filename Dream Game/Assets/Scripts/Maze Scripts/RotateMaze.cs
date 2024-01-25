ad91778d6850946903fc97cb7e517b09b77b37a4
camilee03 <crlee15@asu.edu>
2024-01-24 21:40:50 -0700

    One Level
    - Finished a level
    - Refined rotation
    - Added time.deltatime to rotation so it stops on pause

--- Dream Game/Assets/Scripts/Maze Scripts/RotateMaze.cs
+++ Dream Game/Assets/Scripts/Maze Scripts/RotateMaze.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class RotateMaze : MonoBehaviour
 {
+    private float rotationLeft = 0;
+    private float rotationRight = 0;
+    public float rotationSpeed = 5f;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) { 
-            this.transform.Rotate(0, 0, 5f);
-        }
-            
-        else if (Input.GetKeyDown(KeyCode.DownArrow)) { 
-            this.transform.Rotate(0, 0, -5f);
+        if (Input.GetKeyDown(KeyCode.UpArrow) && rotationLeft < rotationSpeed*10) {
+            rotationLeft+=rotationSpeed;
+            rotationRight = 0;
         }
 
-        
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && rotationRight < rotationSpeed*10) { 
+            rotationRight+=rotationSpeed;
+            rotationLeft = 0;
+        }
+        this.transform.Rotate(0, 0, rotationLeft*Time.deltaTime);
+        this.transform.Rotate(0, 0, -rotationRight*Time.deltaTime);
+
+
     }
 }
